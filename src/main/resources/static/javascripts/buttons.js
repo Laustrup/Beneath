3c6ed5960191b1361113ff48c4a777e5909fa986
Laust <laust.bonnesen@mail.com>
2021-07-26 17:17:55 +0200

    Javascript separated

---
+++ src/main/resources/static/javascripts/buttons.js
@@ -0,0 +1,9 @@
+function onMouseColour(object) {
+    object.style.backgroundColor = 'darkgreen';
+    object.style.color = 'beige';
+}
+
+function offMouseColour(object) {
+    object.style.backgroundColor = 'orange';
+    object.style.color = 'darkgreen';
+}
