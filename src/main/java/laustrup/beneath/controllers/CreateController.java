18ae790de21cede80a92c2253bc3bc4410222ebd
Laust <laust.bonnesen@mail.com>
2021-07-29 12:41:08 +0200

    Checks if inputs are allowed in db lengthwise

--- src/main/java/laustrup/beneath/controllers/CreateController.java
+++ src/main/java/laustrup/beneath/controllers/CreateController.java
@@ -6,22 +6,20 @@ import laustrup.beneath.models.controller_models.Mannequin;
 import laustrup.beneath.models.enums.Gender;
 import laustrup.beneath.repositories.cache.Wallet;
 import laustrup.beneath.services.Creator;
-import laustrup.beneath.services.Print;
+import laustrup.beneath.services.Exception;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.util.Date;
 
 @Controller
 public class CreateController {
 
     private Happening happening = new Happening();
     private Mannequin mannequin = new Mannequin();
+
+    private Exception handler = new Exception();
 
     @PostMapping("/create_new_user")
     public String createNewUser(@RequestParam(name = "name") String name,
@@ -38,7 +36,15 @@ public class CreateController {
                                 @RequestParam(name = "cover_url") String coverUrl,
                                 HttpServletRequest request) {
 
+        String[] inputs = {name,password,email,description,education,work};
         happening.activateSession(request);
+
+        String checkedLength = checkLengths(inputs);
+
+        if(!checkedLength.equals("Length is allowed")) {
+            happening.setAttribute("Exception",checkedLength);
+            return "/";
+        }
 
         User user = new Creator().createUser(name,password,email,gender,isIntoFemales,isIntoMales,isIntoOthers,
                                                 dateOfBirth,description,education,work,coverUrl);
@@ -49,5 +55,21 @@ public class CreateController {
         happening.addAttributes(sessionKeys,sessionValues);
 
         return "redirect:/dashboard-" + user.getName();
+    }
+
+    private String checkLengths(String[] inputs) {
+        String[] checkedLengths = {handler.isLengthAllowedInDb(inputs[0],"username"),
+                                    handler.isLengthAllowedInDb(inputs[1],"user_password"),
+                                    handler.isLengthAllowedInDb(inputs[2],"email"),
+                                    handler.isLengthAllowedInDb(inputs[3],"user_description"),
+                                    handler.isLengthAllowedInDb(inputs[4],"education"),
+                                    handler.isLengthAllowedInDb(inputs[5],"user_work")};
+
+        for (int i = 0; i < checkedLengths.length;i++) {
+            if (!checkedLengths[i].equals("Length is allowed")) {
+                return checkedLengths[i];
+            }
+        }
+        return "Length is allowed";
     }
 }
